c46386719b752a6d72c8847392ad8f3477bf850d
CaptainGones <jordan.feger67@gmail.com>
2017-01-12 16:55:58 +0100

    Ajout des Rows & Circle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Row extends Component{
+    render(){
+        return(
+            <div className="Row">
+                <Circle />
+                <Circle /> 
+                <Circle /> 
+                <Circle /> 
+            </div>
+        )
+    }
+}
+
+class Circle extends Component{
+    render(){
+        return(
+            <div className="Circle"></div>
+        )
+    }
+}
+
 class App extends Component{
     render(){
+        
         return (
             <div className="App">
                 <h1>Mastermind</h1>
                 <div className="Board">
-                    
+                    {[<Row />]}
                 </div>
             </div>
         );
